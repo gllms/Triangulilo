c4454f53c25e5edaa02318280fb30b887fc5e207
gllms <willemvanderdoe@hotmail.com>
2020-10-05 04:14:51 +0200

    Add interactive dots

--- script.js
+++ script.js
@@ -8,13 +8,16 @@ dragula($("#colours"))
 
 let width = 1920
 let height = 1080
+let prevWidth = width
+let prevHeight = height
 let scale = 10
 let variance = 50
 let varianceScale = 100
 let p = 7919
 let fillStyle = "gradient"
-let gradient;
-let rotation = 45;
+let gradient
+let pos1 = { x: 0, y: 0 }
+let pos2 = { x: width, y: height }
 
 let c = $("#result")
 let ctx = c.getContext("2d")
@@ -25,8 +28,19 @@ ctx.strokeWeight = 1
 
 let noise = new SimplexNoise()
 
-updateGradient();
+let factor = 1
+let padding = 50
+let dotSize = 16
+let dragging = false
+let dragElement = undefined
+
+updateGradient()
 draw()
+updateCanvas()
+$("#dot1").style.display = "block"
+$("#dot2").style.display = "block"
+
+window.addEventListener("resize", updateCanvas)
 
 function draw() {
   let size = (width + height) / 2 / 100 * scale
@@ -65,14 +79,7 @@ function draw() {
 }
 
 function updateGradient() {
-  let l = Math.sqrt((width/2)**2+(height/2)**2)
-  let t1 = (rotation+180) * (Math.PI/180)
-  let t2 = rotation * (Math.PI/180)
-  let x1 = l * Math.cos(t1) + width / 2
-  let y1 = l * Math.sin(t1) + height / 2
-  let x2 = l * Math.cos(t2) + width / 2
-  let y2 = l * Math.sin(t2) + height / 2
-  gradient = sctx.createLinearGradient(x1, y1, x2, y2)
+  gradient = sctx.createLinearGradient(pos1.x, pos1.y, pos2.x, pos2.y)
   let a = 1 / ($$("#colours input").length - 1);
   [...$$("#colours input")].forEach(function (e, i) {
     gradient.addColorStop(a * i, e.value)
@@ -80,11 +87,6 @@ function updateGradient() {
   sctx.fillStyle = gradient
   sctx.fillRect(0, 0, width, height)
   draw()
-}
-
-function updateRotation(e) {
-  rotation = parseFloat(e.value) || 0
-  updateGradient()
 }
 
 function addColour() {
@@ -95,24 +97,28 @@ function addColour() {
 function removeColour() {
   let ee = $$("#colours input")
   if (ee.length > 2) {
-    let e = ee[ee.length-1]
+    let e = ee[ee.length - 1]
     e.parentElement.removeChild(e)
     updateGradient()
   }
 }
 
 function updateWidth(e) {
+  prevWidth = width
   width = parseInt(e.value) || 0
   c.setAttribute("width", width)
   s.setAttribute("width", width)
+  updateCanvas()
   updateGradient()
   draw()
 }
 
 function updateHeight(e) {
+  prevHeight = height
   height = parseInt(e.value) || 0
   c.setAttribute("height", height)
   s.setAttribute("height", height)
+  updateCanvas()
   updateGradient()
   draw()
 }
@@ -137,6 +143,79 @@ function updateSeed(e) {
   else noise = new SimplexNoise(e.value)
   draw()
 }
+
+
+function updateCanvas() {
+  let w = window.innerWidth - $("#toolbar").clientWidth - 2 * padding
+  let h = window.innerHeight - 2 * padding
+  if (w / h > width / height) {
+    c.style.height = h + "px"
+    factor = h / height
+    c.style.width = factor * width + "px"
+  } else {
+    c.style.width = w + "px"
+    factor = w / width
+    c.style.height = factor * height + "px"
+  }
+
+  if (prevWidth == 0) pos2.x += width
+  else pos2.x *= width / prevWidth
+  if (prevHeight == 0) pos2.y += height
+  else pos2.y *= height / prevHeight
+
+  prevWidth = width
+  prevHeight = height
+
+  let r = $("#result").getBoundingClientRect()
+
+  let x1 = r.x + padding - 8 + pos1.x * factor
+  let y1 = r.y + padding - 8 + pos1.y * factor
+  let x2 = r.x + padding - 8 + pos2.x * factor
+  let y2 = r.y + padding - 8 + pos2.y * factor
+
+  $("#dot1").style.left = x1 + "px"
+  $("#dot1").style.top = y1 + "px"
+  $("#dot2").style.left = x2 + "px"
+  $("#dot2").style.top = y2 + "px"
+}
+
+document.body.addEventListener("mousedown", function (e) {
+  if (e.target.classList.contains("dot")) {
+    dragElement = e.target
+    dragging = true
+    e.preventDefault()
+  }
+})
+
+document.body.addEventListener("mouseup", function (e) {
+  dragging = false
+})
+
+document.body.addEventListener("mousemove", function (e) {
+  if (dragging) {
+    let r = $("#result").getBoundingClientRect()
+    let mx = e.pageX
+    let my = e.pageY
+    if (Math.abs(mx - padding - r.x) < 10) mx = r.x + padding
+    if (Math.abs(mx + padding - r.right) < 10) mx = r.right - padding
+    if (Math.abs(my - padding - r.y) < 10) my = r.y + padding
+    if (Math.abs(my + padding - r.bottom) < 10) my = r.bottom - padding
+    dragElement.style.left = mx - dotSize / 2 + "px"
+    dragElement.style.top = my - dotSize / 2 + "px"
+
+    let x = (mx - r.x - padding + 8) / factor - dotSize
+    let y = (my - r.y - padding + 8) / factor - dotSize
+    if (dragElement.id == "dot1") {
+      pos1.x = x || 0
+      pos1.y = y || 0
+    } else {
+      pos2.x = x || 0
+      pos2.y = y || 0
+    }
+    updateGradient()
+    e.preventDefault()
+  }
+})
 
 function n(x, y) {
   return noise.noise2D(x / 100 * (varianceScale / 1000 * scale), y / 100 * (varianceScale / 1000 * scale))
